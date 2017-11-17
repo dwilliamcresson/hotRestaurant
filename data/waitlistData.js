8853c17e66cd969f5483244699379e7361e6988e
Will <cresson.will@gmail.com>
2017-11-16 19:52:17 -0500

    mo fixes

--- data/waitlistData.js
+++ data/waitlistData.js
@@ -3,7 +3,8 @@ var waitlistArray = [{
     name: "Elena Wasnak",
     time: "8.00PM",
     date: "11/15/2018",
-    specialOccasion: true
+    phone: "555-867-5309",
+    email: "no@no.no"
 }];
 
 module.export = waitlistArray;
